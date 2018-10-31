388ceb07dc76f7bceeb932d590ce107e7d1cc0de
tyler rasmussen <ty.sundon@gmail.com>
2018-10-30 21:52:29 -0600

    update

--- Assets/Util.cs
+++ Assets/Util.cs
@@ -74,21 +74,22 @@ public static class Util {
                 float dot = Vector3.Dot(rayDirection, vectorToEnemy);
                 Debug.DrawRay(rayOrigin, 10 * rayDirection, Color.yellow);
                 Debug.Log("dot " + Vector3.Dot(rayDirection, vectorToEnemy));
-                if (dot > .985)
+                if (dot > .95)
                 {
                     //snap to target
                     float dToEnemy = Mathf.Abs(e.transform.position.x - self.position.x);
                     Debug.Log("dist to enemy: " + dToEnemy);
-                    if(dToEnemy < 8 && dot * dToEnemy < smallestDistance)
+                    float distance = Vector3.Distance(rayOrigin, point);
+                    float angle = Vector3.Angle(rayDirection, point - rayOrigin);
+                    float dist = (distance * Mathf.Sin(angle * Mathf.Deg2Rad));
+                    Debug.Log("dist " + dist);
+                    if (dist < .5f && dToEnemy < 12 && dot * dToEnemy < smallestDistance)
                     {
                         vectToEnemy = vectorToEnemy;
                         smallestDistance = dToEnemy;
                     }
                 }
-                float distance = Vector3.Distance(rayOrigin, point);
-                float angle = Vector3.Angle(rayDirection, point - rayOrigin);
-                float dist = (distance * Mathf.Sin(angle * Mathf.Deg2Rad));
-                Debug.Log("dist " + dist);
+                
 
             }
             _rightStickInputActive = Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f;
